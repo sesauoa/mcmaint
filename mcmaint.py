7fe6afbd0f23002966e1514a70c48d8007caec81
Andrew Hughson <andrew@hughson.me>
2013-07-30 15:35:04 +1200

    Compress backups

--- mcmaint.py
+++ mcmaint.py
@@ -33,13 +33,14 @@ def directory_size(path):
 
 def prune_backups(backup_dir, max_backup_size):
     backups = list(reversed(sorted(os.listdir(backup_dir))))
+    print(backups)
 
     while(backups and directory_size(backup_dir) > max_backup_size):
         backup = backups.pop()
-        shutil.rmtree(os.path.join(backup_dir, backup))
+        os.remove(os.path.join(backup_dir, backup))
 
 
-def backup_worlds(server_dir, world_name, backup_dir, max_backup_size):
+def backup_world(server_dir, world_name, backup_dir):
     world_dir = os.path.join(server_dir, world_name)
     backup_name = "{}_{}".format(world_name, datetime.datetime.now().date().isoformat())
     backup_dest = os.path.join(backup_dir, backup_name)
@@ -47,14 +48,16 @@ def backup_worlds(server_dir, world_name, backup_dir, max_backup_size):
     if (os.path.exists(world_dir) and not os.path.exists(backup_dest)):
         shutil.copytree(world_dir, backup_dest)
 
-    if (directory_size(backup_dir) > max_backup_size):
-        prune_backups(backup_dir, max_backup_size)
-
     return backup_dest
 
 
-def generate_maps(world_dir, overviewer_dir):
+def generate_map(world_dir, overviewer_dir):
     subprocess.call('overviewer.py {} {}'.format(world_dir, overviewer_dir), shell=True)
+
+
+def compress_backup(backup_name, backup_dest):
+    subprocess.call('zip -r {} {}'.format(backup_dest + '.zip', backup_dest), shell=True)
+    shutil.rmtree(backup_dest)
 
 
 def main():
@@ -65,8 +68,10 @@ def main():
     with open(args.config) as f:
         cfg = json.load(f)
 
-    backup_location = backup_worlds(cfg['server_dir'], cfg['world_name'], cfg['backup_dir'], cfg['max_backup_size'])
-    generate_maps(backup_location, cfg['overviewer_dir'])
+    backup_dest = backup_world(cfg['server_dir'], cfg['world_name'], cfg['backup_dir'])
+    generate_map(backup_dest, cfg['overviewer_dir'])
+    compress_backup(backup_dest)
+    prune_backups(cfg['backup_dir'], cfg['max_backup_size'])
 
 
 if __name__ == '__main__':
